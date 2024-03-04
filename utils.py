8167f376426f88943115e1673f4fea7ea2ef1219
purna <147084715+purnakaizoku@users.noreply.github.com>
2024-03-04 17:14:45 +0530

    utils.py

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@
 import openai
 
 # Set your OpenAI API key
-openai.api_key = ""
+openai.api_key = "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"
 
 def generate_description(input):
     messages = [
